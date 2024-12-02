5ac2fb513f7f2e0500e847ce36efa7d4a9941b26
이서희 책임/LG-NET팀 <seohee727@lgcns.com>
2024-12-02 17:48:47 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
+app = FastAPI(
+    title="FastAPI - Hello World code",
+    description="This is the Hello World of FastAPI.",
+    version="1.0.0",
+)
+
+# Pydantic모듈의 BaseModel Item 클래스 정의(Requsest Body로 받을 데이터를 정의)
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+@app.get("/")
+def hello_world():
+    return "Hello World!!"
+
+@app.get("/get_test/{input_val}")
+def get_test1(input_val):
+    return {"values": input_val}
+
+@app.get("/get_test2/{input_val}")
+def get_test2(input_val: int, q: str):
+    return {"item_id": input_val, "q": q}
+
+@app.post('/post_test')
+def post_test(item: Item):
+    return item
---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+# pip install pytest
+import pytest
+from fastapi.testclient import TestClient
+from src.main import app
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+def test_hello_world(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.status_code != 500
+    assert response.json() == "Hello World!!"
