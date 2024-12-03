027a0be8fa0b465cad5014d77b07711d14588673
이서희 책임/LG-NET팀 <seohee727@lgcns.com>
2024-12-03 15:14:23 +0900

    hello to fastapi

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def hello_world():
-    return "Hello World!!"
+    return "FastAPI World!!"
 
 @app.get("/get_test/{input_val}")
 def get_test1(input_val):
